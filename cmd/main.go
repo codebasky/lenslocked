b1d0adddc00722ab4f64315d3d1a33eae42e28b9
codebasky <baskar.shanmugam.career@gmail.com>
2023-02-21 06:43:34 +0530

    Added tailwind css changes

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 
 	"github.com/codebasky/lenslocked/controllers"
+	"github.com/codebasky/lenslocked/templates"
 	"github.com/codebasky/lenslocked/views"
 )
 
 func main() {
 	fmt.Println("Starting web development")
-	http.HandleFunc("/", controllers.StaticHandler(views.Must(views.Parse("home.gohtml"))))
-	http.HandleFunc("/contact", controllers.StaticHandler(views.Must(views.Parse("contact.gohtml"))))
-	http.HandleFunc("/faq", controllers.StaticHandler(views.Must(views.Parse("faq.gohtml"))))
+	http.HandleFunc("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+	http.HandleFunc("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+	http.HandleFunc("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 	http.ListenAndServe(":3000", nil)
 }
