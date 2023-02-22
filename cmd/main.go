5bc27308bf0d8be8ffa6afd0c0968e266c7ba97c
codebasky <baskar.shanmugam.career@gmail.com>
2023-02-22 07:37:18 +0530

    Added chi router support

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,16 @@ import (
 	"github.com/codebasky/lenslocked/controllers"
 	"github.com/codebasky/lenslocked/templates"
 	"github.com/codebasky/lenslocked/views"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	fmt.Println("Starting web development")
-	http.HandleFunc("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
-	http.HandleFunc("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
-	http.HandleFunc("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
-	http.HandleFunc("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
-	http.ListenAndServe(":3000", nil)
+	r := chi.NewRouter()
+
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+	http.ListenAndServe(":3000", r)
 }
